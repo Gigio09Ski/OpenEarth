a5c9f4fabfb5be73bcc11e532d1c5c5eaa344ae9
Gigio09Ski <74615467+Gigio09Ski@users.noreply.github.com>
2022-11-25 10:57:26 +0100

    Updata 0.0.2 Tattico

---
+++ Codice Advanced/Codice Advanced.ino
@@ -0,0 +1,80 @@
+#include <FastLED.h>
+
+#define DATA_PIN 7
+#define NUM_LEDS 60
+#define COLOR_ORDER GRB
+#define CHIPSET WS2812
+#define VOLTS 5
+#define MAX_AMPS 500
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER> (leds, NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps (VOLTS, MAX_AMPS);
+  FastLED.clear();
+  FastLED.show();
+}
+
+void loop() {
+leds [0] = CRGB (255, 30, 0);
+leds [1] = CRGB (255, 30, 0);
+leds [2] = CRGB (255, 30, 0);
+leds [3] = CRGB (255, 30, 0);
+leds [4] = CRGB (255, 30, 0);
+leds [5] = CRGB (255, 30, 0);
+leds [6] = CRGB (255, 30, 0);
+leds [7] = CRGB (255, 30, 0);
+leds [8] = CRGB (255, 30, 0);
+leds [9] = CRGB (255, 30, 0);
+leds [10] = CRGB (255, 30, 0);
+leds [11] = CRGB (255, 30, 0);
+leds [12] = CRGB (255, 30, 0);
+leds [13] = CRGB (255, 30, 0);
+leds [14] = CRGB (255, 30, 0);
+leds [15] = CRGB (255, 30, 0);
+leds [16] = CRGB (255, 30, 0);
+leds [17] = CRGB (255, 30, 0);
+leds [18] = CRGB (255, 30, 0);
+leds [19] = CRGB (255, 30, 0);
+leds [20] = CRGB (255, 30, 0);
+leds [21] = CRGB (255, 30, 0);
+leds [22] = CRGB (255, 30, 0);
+leds [23] = CRGB (255, 30, 0);
+leds [24] = CRGB (255, 30, 0);
+leds [25] = CRGB (255, 30, 0);
+leds [26] = CRGB (255, 30, 0);
+leds [27] = CRGB (255, 30, 0);
+leds [28] = CRGB (255, 30, 0);
+leds [29] = CRGB (255, 30, 0);
+leds [30] = CRGB (255, 30, 0);
+FastLED.show ();
+delay (500);
+leds [32] = CRGB (255, 7, 0);
+leds [33] = CRGB (255, 7, 0);
+leds [34] = CRGB (255, 7, 0);
+leds [35] = CRGB (255, 7, 0);
+leds [36] = CRGB (255, 7, 0);
+leds [37] = CRGB (255, 7, 0);
+leds [38] = CRGB (255, 7, 0);
+leds [39] = CRGB (255, 7, 0);
+leds [40] = CRGB (255, 7, 0);
+leds [41] = CRGB (255, 7, 0);
+leds [42] = CRGB (255, 7, 0);
+leds [43] = CRGB (255, 7, 0);
+leds [44] = CRGB (255, 7, 0);
+leds [45] = CRGB (255, 7, 0);
+leds [46] = CRGB (255, 7, 0);
+leds [47] = CRGB (255, 7, 0);
+leds [48] = CRGB (255, 7, 0);
+leds [49] = CRGB (255, 7, 0);
+leds [50] = CRGB (255, 7, 0);
+FastLED.show ();
+delay (500);
+leds [55] = CRGB (255, 0, 0);
+leds [56] = CRGB (255, 0, 0);
+leds [57] = CRGB (255, 0, 0);
+leds [58] = CRGB (255, 0, 0);
+FastLED.show ();
+delay (500);
+}
